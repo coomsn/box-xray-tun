580f4866baaf6bbb8ed1bcf58ba8d743c2337f97
coomsn <61256281+coomsn@users.noreply.github.com>
2024-11-29 13:11:17 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
 zip -r -o -X -ll box-xray-tun-$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'build.sh' -x '.github/*' -x 'update.json' -x '.gitattributes'
