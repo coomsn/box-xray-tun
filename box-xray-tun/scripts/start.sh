1f5636148ec94009d91f8b83e94d91b254d71b5f
coomsn <acstudycn@gmail.com>
2024-11-29 13:44:17 +0800

    Update start.sh

--- box-xray-tun/scripts/start.sh
+++ box-xray-tun/scripts/start.sh
@@ -2,7 +2,7 @@
 clear
 scripts=$(realpath $0)
 scripts_dir=$(dirname ${scripts})
-module_dir="/data/adb/modules/box_tun-module"
+module_dir="/data/adb/modules/box-xray-tun"
 
 source ${scripts_dir}/box-xray-tun.service
 if [ ! -f "${module_dir}/disable" ]; then
@@ -12,23 +12,23 @@ else
 fi
 
 # environment variables
-export PATH="/data/adb/magisk:$PATH:/system/bin"
+export PATH="/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH"
 
 # Check if the disable file does not exist, then run the proxy
 if [ ! -f "${module_dir}/disable" ]; then
   start_tun # >/dev/null 2>&1
 fi
 
-start_box_tun.inotify() {
+start_inotifyd() {
   PIDs=($(busybox pidof inotifyd))
   for PID in "${PIDs[@]}"; do
-    if grep -q "box_tun.inotify" "/proc/$PID/cmdline"; then
+    if grep -q "box-xray-tun.inotify" "/proc/$PID/cmdline"; then
       kill -9 "$PID"
     fi
   done
-  inotifyd "${scripts_dir}/box_tun.inotify" "${module_dir}" >/dev/null 2>&1 &
+  inotifyd "${scripts_dir}/box-xray-tun.inotify" "${module_dir}" >/dev/null 2>&1 &
 }
 
-start_box_tun.inotify
+start_inotifyd
 
 # version2.0
