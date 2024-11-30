e9b8fb4db9ec9b85402b9ce620e1ecacc32d383f
coomsn <acstudycn@gmail.com>
2024-11-30 21:17:48 +0800

    Update box-xray-tun.sh

--- box-xray-tun.sh
+++ box-xray-tun.sh
@@ -1,3 +1,13 @@
 #!/system/bin/sh
+
 scripts_dir="/data/adb/box-xray-tun/scripts"
-(until [ "$(getprop sys.boot_completed)" -eq 1 ]; do sleep 4; done; "${scripts_dir}/start.sh") &
+(
+until [ "$(getprop sys.boot_completed)" -eq 1 ]; do
+    sleep 4
+done
+
+chmod 755 "${scripts_dir}/start.sh"
+
+"${scripts_dir}/start.sh" &
+)
+
