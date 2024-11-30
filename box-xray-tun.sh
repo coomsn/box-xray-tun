803662c932ae4bf6b5fa46b891f5b81c763e7f99
coomsn <61256281+coomsn@users.noreply.github.com>
2024-11-30 16:12:37 +0800

    Update box-xray-tun.sh

--- box-xray-tun.sh
+++ box-xray-tun.sh
@@ -1,3 +1,3 @@
 #!/system/bin/sh
 scripts_dir="/data/adb/box-xray-tun/scripts"
-(until [ "$(getprop sys.boot_completed)" -eq 1 ]; do sleep 3; done; "${scripts_dir}/start.sh") &
+(until [ "$(getprop sys.boot_completed)" -eq 1 ]; do sleep 4; done; "${scripts_dir}/start.sh") &
