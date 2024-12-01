125942cfdf2e46f3927bd63a69699d8424212079
coomsn <acstudycn@gmail.com>
2024-12-02 07:57:24 +0800

    update

--- box-xray-tun/scripts/start.sh
+++ box-xray-tun/scripts/start.sh
@@ -6,25 +6,29 @@ module_dir="/data/adb/modules/box-xray-tun"
 # Environment variable settings
 export PATH="/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH"
 
+proxy_service() {
+if [ ! -f "${module_dir}/disable" ]; then
+  log Info "Module Enabled"
+  log Info "start_tun"
+  ${scripts_dir}/box-xray-tun.service enable >/dev/null 2>&1
+else
+  log Warn "Module Disabled"
+  log Warn "stop_tun"
+  ${scripts_dir}/box-xray-tun.service disable >/dev/null 2>&1
+fi
+}
+
 start_inotifyd() {
   PIDs=($(busybox pidof inotifyd))
   for PID in "${PIDs[@]}"; do
-    if grep -q "box-xray-tun.inotify" "/proc/$PID/cmdline"; then
+    if grep -q "${scripts_dir}/box-xray-tun.inotify" "/proc/$PID/cmdline"; then
       kill -9 "$PID"
-	  break
     fi
   done
   inotifyd "${scripts_dir}/box-xray-tun.inotify" "${module_dir}" >/dev/null 2>&1 &
 }
 
+proxy_service
 start_inotifyd
 
 # {version:2.1}
-
-source ${scripts_dir}/box-xray-tun.service
-
-if [ ! -f "${module_dir}/disable" ]; then
-  log Info "Module Enabled"
-else
-  log Warn "Module Disabled"
-fi
